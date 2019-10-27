9369ec16fa02c089199dd73439c7a9bf6b810b2a
aakashRajur <aakashrajur16@gmail.com>
2019-10-27 20:13:43 +0530

    script to initialize kube master node

---
+++ kmaster.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+/bin/bash /home/vagrant/scripts/disable-selinux.sh
+/bin/bash /home/vagrant/scripts/disable-swap.sh
+/bin/bash /home/vagrant/scripts/disable-firewall.sh
+/bin/bash /home/vagrant/scripts/config-ssh.sh
+/bin/bash /home/vagrant/scripts/config-sysctl.sh
+/bin/bash /home/vagrant/scripts/install-docker.sh
+/bin/bash /home/vagrant/scripts/install-kubernetes.sh
+/bin/bash /home/vagrant/scripts/start-kmaster.sh
